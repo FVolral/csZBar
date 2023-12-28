48dc7d70e65e861f9dc31fac360cd32bed4ad565
Volral François <fvolral@awegroup.be>
2023-12-28 13:56:40 +0100

    test fix ZBarCameraSimulator

--- ios/ZBarSDK/ZBarCameraSimulator.h
+++ ios/ZBarSDK/ZBarCameraSimulator.h
@@ -21,25 +21,25 @@
 //  http://sourceforge.net/projects/zbar
 //------------------------------------------------------------------------
 
-@class ZBarReaderView;
+// @class ZBarReaderView;
 
-// hack around missing simulator support for AVCapture interfaces
+// // hack around missing simulator support for AVCapture interfaces
 
-@interface ZBarCameraSimulator
-    : NSObject
-    < UINavigationControllerDelegate,
-      UIImagePickerControllerDelegate,
-      UIPopoverControllerDelegate >
-{
-    UIViewController *viewController;
-    ZBarReaderView *readerView;
-    UIImagePickerController *picker;
-    UIPopoverController *pickerPopover;
-}
+// @interface ZBarCameraSimulator
+//     : NSObject
+//     < UINavigationControllerDelegate,
+//       UIImagePickerControllerDelegate,
+//       UIPopoverControllerDelegate >
+// {
+//     UIViewController *viewController;
+//     ZBarReaderView *readerView;
+//     UIImagePickerController *picker;
+//     UIPopoverController *pickerPopover;
+// }
 
-- (id) initWithViewController: (UIViewController*) viewController;
-- (void) takePicture;
+// - (id) initWithViewController: (UIViewController*) viewController;
+// - (void) takePicture;
 
-@property (nonatomic, assign) ZBarReaderView *readerView;
+// @property (nonatomic, assign) ZBarReaderView *readerView;
 
-@end
+// @end
